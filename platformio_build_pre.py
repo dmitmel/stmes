07ec9b3230ddd7920a2dc49813f6ce50bdc98acc
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2023-05-15 00:43:31 +0200

    enable hard-float math (this will hopefully come in handy later)

--- platformio_build_pre.py
+++ platformio_build_pre.py
@@ -12,3 +12,6 @@ if normalize_bool_option(env.GetProjectOption("arm_semihosting", False)):
     LIBS=["rdimon"],
     CPPDEFINES=["ARM_SEMIHOSTING_ENABLE"],
   )
+
+fpu_flags = ["-mfloat-abi=hard", "-mfpu=fpv4-sp-d16"]
+env.Append(CCFLAGS=fpu_flags, ASFLAGS=fpu_flags, LINKFLAGS=fpu_flags)
