53884f0932863a744c22cc11ee01bac8f40557bb
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2023-08-27 13:52:20 +0200

    use an interrupt for handling overflows of the systime timer

--- src/stmes/kernel/time.c
+++ src/stmes/kernel/time.c
@@ -7,11 +7,12 @@
 #include <stm32f4xx_ll_tim.h>
 
 static TIM_HandleTypeDef hwtimer;
+static volatile u32 systime_epoch;
 
 void hwtimer_init(void) {
   __HAL_RCC_TIM5_CLK_ENABLE();
 
-  HAL_NVIC_SetPriority(TIM5_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(TIM5_IRQn, 14, 0);
   HAL_NVIC_EnableIRQ(TIM5_IRQn);
 
   u32 apb1_freq = HAL_RCC_GetPCLK1Freq() * (LL_RCC_GetAPB1Prescaler() == RCC_HCLK_DIV1 ? 1 : 2);
@@ -66,7 +67,6 @@ void hwtimer_deinit(void) {
   __HAL_RCC_TIM5_CLK_DISABLE();
 }
 
-// TODO: change to ticks
 u32 hwtimer_read(void) {
   return LL_TIM_GetCounter(TIM5);
 }
@@ -81,28 +81,28 @@ void TIM5_IRQHandler(void) {
     LL_TIM_ClearFlag_CC1(hwtimer);
     task_yield_from_isr();
   }
+  if (unlikely(LL_TIM_IsActiveFlag_UPDATE(hwtimer))) {
+    LL_TIM_ClearFlag_UPDATE(hwtimer);
+    // The addition doesn't need to be atomic since this interrupt is the only
+    // piece of code modifying this variable.
+    systime_epoch += 1;
+  }
 }
 
+// NOTE: This function shouldn't be called from within interrupt handlers, as
+// it will miss overflows of the hardware timer used for timekeeping!
 Systime systime_now(void) {
-  TIM_TypeDef* hwtimer = TIM5;
-  u32 primask = __get_PRIMASK();
-  __disable_irq();
+  // u32 irq = interrupt_number();
+  // ASSERT(irq == 0 || irq == SVCall_IRQn + 16 || irq == PendSV_IRQn + 16);
   while (true) {
-    static volatile u32 systime_epoch;
     u32 epoch = systime_epoch;
-    u32 hwtime = LL_TIM_GetCounter(hwtimer);
-    // Check if the timer has overflown before calling this function or while
-    // we were reading the counter values in the previous two lines.
-    // TODO: Can this be safely down without establishing a critical section?
-    if (unlikely(LL_TIM_IsActiveFlag_UPDATE(hwtimer))) {
-      LL_TIM_ClearFlag_UPDATE(hwtimer);
-      // TODO: Do something about overflows!
-      systime_epoch += 1;
-      continue;
+    u32 hwtime = LL_TIM_GetCounter(TIM5);
+    u32 epoch2 = systime_epoch;
+    // Check that the timer has not overflown while we were reading the counter value.
+    if (epoch2 == epoch) {
+      // Now we can be sure that both counters represent the same epoch.
+      return ((u64)epoch << 32) | (u64)hwtime;
     }
-    __set_PRIMASK(primask);
-    // Now we can be sure that both counters represent the same epoch.
-    return ((u64)epoch << 32) | (u64)hwtime;
   }
 }
 
